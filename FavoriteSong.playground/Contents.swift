dc16963dff565ae6bcb3fffbc5301d7f29c07a68
A B M Nasim <abmnasim.ibss@gmail.com>
2021-04-21 00:59:23 +0600

    initialise and print data

--- FavoriteSong.playground/Contents.swift
+++ FavoriteSong.playground/Contents.swift
@@ -1,3 +1,34 @@
+/*
+ * About my favorite song (It's My Life)
+ * Apply some data types of swift
+ */
 import Cocoa
 
-var str = "Hello, playground"
+var artist : String = "Jon Bon Jovi"
+var releasedYear : Int = 2000
+var genre : String = "Pop rock, Arena rock"
+var duration : Double = 4.26
+var title : String = "It's My Life"
+var album : String = "Crush"
+
+// Print the Song title
+print("Title: " + title)
+
+// Print Duration of the Song
+print("Duration: \(duration)")
+
+/*
+  Print Genres
+   1. Pop rock
+   2. Arena rock
+ */
+print("Gemre: " + genre)
+
+// Print Artist Name
+print("Artist: " + artist)
+
+// Print Album
+print("Album: " + album)
+
+// Print Released Year
+print("Released Year: \(releasedYear)")
